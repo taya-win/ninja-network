c6c5050bb15961445e078b7d3d002f31a6b816a4
taya-win <tayawin.tyw@gmail.com>
2024-11-29 21:55:46 +0630

    Components, Attributes & Props

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,7 @@
+@props(['highlight' => false])
+
+<div @class(['highlight' => $highlight, 'card'])>
+    {{$slot}}
+    {{-- <a href="{{$attributes->get('href')}}" class="btn">View Details</a> --}}
+    <a {{$attributes}} class="btn">View Details</a>
+</div>
--- resources/views/ninjas/index.blade.php
+++ resources/views/ninjas/index.blade.php
@@ -7,12 +7,9 @@
     <ul>
         @foreach ($ninjas as $ninja)
         <li>
-            <p>
-                {{$ninja['name']}}
-            </p>
-            <a href="/ninjas/{{$ninja['id']}}">
-                View Details
-            </a>
+            <x-card href="/ninjas/{{$ninja['id']}}" :highlight="$ninja['skills'] > 70">
+                <h3>{{$ninja['name']}}</h3>
+            </x-card>
         </li>
         @endforeach
     </ul>
