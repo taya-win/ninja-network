e29e9b20ab416106a27909c04fae80eaa497782f
taya-win <tayawin.tyw@gmail.com>
2024-11-29 23:00:59 +0630

    Adding CSS & Tailwind

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,24 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Ninja Network</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Ninja Network</title>
+
+  @vite('resources/css/app.css')
 </head>
 <body>
-    <header>
-        <nav>
-            <h1>
-                Ninja Network
-            </h1>
-            <a href="/ninjas">All Ninjas</a>
-            <a href="/ninjas/create">Create New Ninja</a>
-        </nav>
-    </header>
+  <header>
+    <nav>
+      <h1>Ninja Network</h1>
+      <a href="/ninjas">
+        All Ninjas
+      </a>
+      <a href="/ninjas/create">
+        Create New Ninja
+      </a>
+    </nav>
+  </header>
 
-    <main class="container">
-        {{$slot}}
-    </main>
+  <main class="container">
+    {{ $slot }}
+  </main>
+
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,17 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Ninja Network</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Ninja Network</title>
+  @vite('resources/css/app.css')
 </head>
-<body>
-    <h1>Welcome to the Ninja Network</h1>
-    <p>Click the button below to view the list of ninjas.</p>
-    <a href="/ninjas" class="btn">
-        Find Ninjas!
-    </a>
+<body class="text-center px-8 py-12">
+  <h1>Welcome to the Ninja Network</h1>
+  <p>Click the button below to view the list of ninjas.</p>
+
+  <a href="/ninjas" class="btn mt-4 inline-block">
+    Find Ninjas!
+  </a>
 </body>
 </html>
