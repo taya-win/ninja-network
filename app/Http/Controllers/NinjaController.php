9043a562a650078ce6853304e4443b8e494cfde7
taya-win <tayawin.tyw@gmail.com>
2024-11-30 11:09:45 +0630

    Pagination

--- app/Http/Controllers/NinjaController.php
+++ app/Http/Controllers/NinjaController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class NinjaController extends Controller
 {
     public function index() {
-        $ninjas = Ninja::orderBy('created_at', 'desc')->get();
+        $ninjas = Ninja::orderBy('created_at', 'desc')->paginate(10);
 
         return view('ninjas.index', [
             'ninjas' => $ninjas,
--- resources/views/ninjas/index.blade.php
+++ resources/views/ninjas/index.blade.php
@@ -13,4 +13,5 @@
         </li>
         @endforeach
     </ul>
+    {{$ninjas->links()}}
 </x-layout>
