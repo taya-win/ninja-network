c9b9f9bde8f770b47f736424107a56b584b94830
taya-win <tayawin.tyw@gmail.com>
2024-11-30 22:07:31 +0630

    Delete Requests

--- app/Http/Controllers/NinjaController.php
+++ app/Http/Controllers/NinjaController.php
@@ -40,4 +40,12 @@ class NinjaController extends Controller
         return redirect()->route('ninjas.index');
 
     }
+
+    public function destroy($id) {
+        $ninja = Ninja::findOrFail($id);
+
+        $ninja->delete();
+
+        return redirect()->route('ninjas.index');
+    }
 }
